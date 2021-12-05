a19ebe5062754a17194f8d915a44bc69dfe1c049
vabarnikova <v.abarnikova@student.csn.khai.edu>
2021-12-06 00:23:24 +0200

    Modified HelloClass.java

--- src/HelloClass.java
+++ src/HelloClass.java
@@ -1,7 +1,7 @@
 public class HelloClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello, my dear friend!!!");
+        System.out.println("Hello, my dear developer!!!");
     }
-    
+
 }
